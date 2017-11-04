82fe0349d7c58a8d9d33cf1b63c0bbdd93993858
palinom <p.eroshevich@mail.ru>
2017-11-04 11:21:39 +0300

    Add doctrine migrations

---
+++ app/DoctrineMigrations/Version20171104080645.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171104080645 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX tblProductData_strProductCode_uindex ON tblProductData');
+        $this->addSql('ALTER TABLE tblProductData CHANGE stmTimestamp stmTimestamp INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tblProductData CHANGE stmTimestamp stmTimestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX tblProductData_strProductCode_uindex ON tblProductData (strProductCode)');
+    }
+}
